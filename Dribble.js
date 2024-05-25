0c2874092cdefcdba5ec8679358f3dcaac92f0f6
Abhay <abhayd0331@gmail.com>
2024-05-25 23:18:01 +0530

    Done

---
+++ Dribble.js
@@ -0,0 +1,14 @@
+var elem = document.querySelectorAll('.elem')
+
+var data = [
+    {name: "Andrea Jelić", skill:"Digital Designer", tag1:"Brand", tag2: "Web", tag3: "UI"},
+    {name: "Helen Tran", skill:"Lead Product Designer", tag1:"Leadership", tag2: "Product", tag3: "UX"},
+    {name: "Aurélien Salomon", skill:"Digital Designer", tag1:"Mobile", tag2: "Product", tag3: "UX"},
+    {name: "Jesse Showalter", skill:"Design Educator", tag1:"UI", tag2: "Web", tag3: "Mobile"},
+    {name: "Chris Owens", skill:"Creative Director", tag1:"Illustration", tag2: "Mobile", tag3: "UI"},
+]
+
+var clutter = ""
+data.forEach(function(dets){
+    clutter += ``
+})
